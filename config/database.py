3ac28e00eba4182bff808cea97fbb921e6d648cc
itreallyhz <mikapmarquez@gmail.com>
2024-01-23 03:26:46 +0800

    heroku postgres db setup

--- config/database.py
+++ config/database.py
@@ -1,11 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import databases
+import sqlalchemy
 
+#SQLALCHEMY_DATABASE_URL = "postgresql://postgres:database@localhost:5432/Abitbo"
+DATABASE_URL = "postgresql://mhzqmzrmatxeof:e1bc11fbf873e05b304c222d7de46d5937ed49ae0a6b4d63e03137eb4effc4fc@ec2-54-234-13-16.compute-1.amazonaws.com:5432/d851asqogq01o7"
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:database@localhost:5432/Abitbo"
+database = databases.Database(DATABASE_URL)
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+metadata = sqlalchemy.MetaData()
+
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
