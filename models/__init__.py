542fed22984d02c865f6bb5772ed67f5bb2ac81a
itreallyhz <116032456+itreallyhz@users.noreply.github.com>
2024-01-23 00:15:06 +0800

    BitboStandardDep

---
+++ models/__init__.py
@@ -0,0 +1,10 @@
+from config.database import Base, engine
+
+import models.ResidentProfile as ResidentProfile
+import models.BarangayOfficial as BarangayOfficial
+import models.Personnel as Personnel
+import models.Ordinance as Ordinance
+import models.Configuration as Configuration
+import models.User as User
+
+
