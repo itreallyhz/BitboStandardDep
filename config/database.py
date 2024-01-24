e729bd47385d6775c020683f33b0a2c6269508dc
John Chris Cayetano <cayetanojohnchris@gmail.com>
2024-01-24 23:44:56 +0800

    Update database.py

--- config/database.py
+++ config/database.py
@@ -11,7 +11,7 @@ load_dotenv()
 
 #SQLALCHEMY_DATABASE_URL = "postgresql://postgres:database@localhost:5432/Abitbo"
 # DATABASE_URL = "postgresql://mhzqmzrmatxeof:e1bc11fbf873e05b304c222d7de46d5937ed49ae0a6b4d63e03137eb4effc4fc@ec2-54-234-13-16.compute-1.amazonaws.com:5432/d851asqogq01o7"
-DATABASE_URL = os.getenv('DATABASE_URL')
+DATABASE_URL = os.getenv('DATABASE_URL_KEY')
 
 database = databases.Database(DATABASE_URL)
 
