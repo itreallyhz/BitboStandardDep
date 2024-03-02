ccc267d23d51a1ecab345a04aef73580eb414964
itreallyhz <mikapmarquez@gmail.com>
2024-03-02 20:04:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from config.database import engine
 
 app = FastAPI(
     title="Bitbo Standard",
-    version="0.0.3",
+    version="0.0.4",
 )
 
 # Allow all origins during development. In production, specify your actual frontend URL.
